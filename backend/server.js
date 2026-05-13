d91bd35975e3fd8f0cba707e3c5ed61f0f942f58
harish-kush <h.kush2005@gmail.com>
2026-05-13 11:46:00 +0530

    updated socket cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://yourfrontend.netlify.app"
+      "https://resolve-ai-theta.vercel.app"
     ],
     methods: ["GET", "POST"],
     credentials: true
